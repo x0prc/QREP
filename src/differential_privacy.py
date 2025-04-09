56e70fcb6dc6f2d8224b7560dca62ffaaac56187
x0prc <c0ldheat@protonmail.com>
2025-04-09 22:59:15 +0530

    feat: added funcs for trainer, analysis and processing data

--- src/differential_privacy.py
+++ src/differential_privacy.py
@@ -4,6 +4,7 @@ from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassifica
 from stylegan2_pytorch import Trainer
 import syft as sy
 from typing import List, Dict
+from this import d
 
 class ContextAwareDP:
     def __init__(self, epsilon_base: float = 0.1):
@@ -25,3 +26,19 @@ class ContextAwareDP:
                 tokenizer=self.tokenizer,
                 framework="pt"
             )
+
+    def _init_gan_trainer(self):
+        """Initialize GAN trainer"""
+        self.gan_trainer = Trainer(
+            name = "health_records_synth",
+            data = "./healthcare_dataset",
+            results_dir = "./gan_results",
+            models_dir = "./gan_models",
+            batch_size = 8,
+            gradient_accumulate_every = 4,
+            image_size = 256,
+            network_capacity = 18
+        )
+
+    def calculate_privacy_budget(self, context_score: int, diversity_metric: float) -> float:
+        return (self.epsilon_base * math.pow(1 + context_score/10, -1) + diversity_metric/5)
