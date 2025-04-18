3c75e0f5fd00e9023257ac0d448669e9fb2267a4
PRC <105520102+x0prc@users.noreply.github.com>
2025-04-18 10:46:42 +0530

    fix: add dir path

--- tests/test_DP.py
+++ tests/test_DP.py
@@ -9,6 +9,11 @@ Tests:
 import numpy as np
 import torch
 import pytest
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from src.differential.differential_privacy import ContextAwareDP
 
 @pytest.fixture
